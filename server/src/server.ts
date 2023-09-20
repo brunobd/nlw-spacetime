ad7d023dd904b33e22b84bec35c4ae88ade18c4f
brunobd <bbduarte@live.com>
2023-09-20 15:14:44 -0300

    add upload route and static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,23 @@ import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import 'dotenv/config'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import fastifyStatic from '@fastify/static'
+import { resolve } from 'path'
 const app = fastify()
 app.register(jwt, {
   secret: 'spacetime',
 })
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+app.register(multipart)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 app.register(authRoutes)
 app.register(cors, {
   origin: true,
